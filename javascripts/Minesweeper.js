114af35f5ae50ddd97fcdf3f6abb2b2afcc389c9
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2015-09-19 16:31:49 +0600

    Add Tiles to model

--- javascripts/Minesweeper.js
+++ javascripts/Minesweeper.js
@@ -1,6 +1,21 @@
 
 'use strict';
 
+function Tile() {
+  this.value  = 0;
+  this.marked = false;
+  this.opened = false;
+}
+
+Tile.prototype.open = function() {
+  this.opened = true;
+  return this.value;
+}
+
+Tile.prototype.mark = function() {
+  this.marked = true;
+}
+
 function Minesweeper(w, h, n) {
   this.w = w;
   this.h = h;
@@ -9,14 +24,7 @@ function Minesweeper(w, h, n) {
   for (var i = 0; i < w; ++i) {
     this.map[i] = [];
     for (var j = 0; j < h; ++j) {
-      this.map[i][j] = 0;
-    }
-  }
-  this.marks = [];
-  for (var i = 0; i < w; ++i) {
-    this.marks[i] = [];
-    for (var j = 0; j < h; ++j) {
-      this.marks[i][j] = 0;
+      this.map[i][j] = new Tile();
     }
   }
   this.generate();
@@ -24,10 +32,10 @@ function Minesweeper(w, h, n) {
 
 Minesweeper.prototype.open = function(x, y) {
   if (0 <= x && x < this.w && 0 <= y && y < this.h) {
-    if(this.map[x][y] === -1) {
+    if(this.map[x][y].open() === -1) {
       this.on_mine_open(x, y);
     } else {
-      this.on_tile_open(x, y, this.map[x][y]);
+      this.on_tile_open(x, y, this.map[x][y].value);
     }
   }
 };
@@ -40,7 +48,7 @@ Minesweeper.prototype.on_tile_open = function(x, y, value) {
 
 Minesweeper.prototype.mark = function(x, y) {
   if (0 <= x && x < this.w && 0 <= y && y < this.h) {
-    return this.mark[x][y] = 1;
+    this.map[x][y].mark();
   }
 };
 
@@ -48,9 +56,9 @@ Minesweeper.prototype.is_complete = function() {
   var count = 0;
   for (var i = 0; i < w; ++i) {
     for (var j = 0; j < h; ++j) {
-      if (this.marks[i][j] === 1) {
+      if (this.map[i][j].marked === true) {
         count++;
-        if (this.map[i][j] !== -1) {
+        if (this.map[i][j].value !== -1) {
           return false;
         }
       }
@@ -61,12 +69,12 @@ Minesweeper.prototype.is_complete = function() {
 
 Minesweeper.prototype.increment = function(x, y) {
   if (0 <= x && x < this.w && 0 <= y && y < this.h && this.map[x][y] !== -1) {
-    this.map[x][y] += 1;
+    this.map[x][y].value += 1;
   }
 };
 
 Minesweeper.prototype.place = function(x, y) {
-  this.map[x][y] = -1;
+  this.map[x][y].value = -1;
 
   this.increment(x - 1, y - 1);
   this.increment(x - 1, y);
@@ -85,7 +93,7 @@ Minesweeper.prototype.generate = function() {
   while(i < this.n) {
     var x = getRandomInteger(0, this.w);
     var y = getRandomInteger(0, this.h);
-    if(this.map[x][y] !== -1) {
+    if(this.map[x][y].value !== -1) {
       this.place(x, y);
       i += 1;
     }
